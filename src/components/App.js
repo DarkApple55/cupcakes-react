7bab2622116f7eafb08dac37b4a359885e680af2
DarkApple55 <axelcy@gmail.com>
2022-12-07 02:20:33 -0300

    Estructura Inicial + Header

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../styles/App.css"
+import Header from "./sections/Header"
+
+const App = () => {
+
+  return(
+    <Header/>
+  )
+}
+
+export default App;
---
+++ src/components/sections/Header.js
@@ -0,0 +1,20 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../styles/Header.css"
+
+const Header = () => {
+  return(
+    <nav className="navbar navbar-expand-lg bg-light Header-header">
+    <div className="container-fluid Header-container">
+      <a className="navbar-brand" href="/">Inicio</a>
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item"><a className="nav-link active" href="/Cupcakes">Cupcakes</a></li>
+          <li className="nav-item"><a className="nav-link active" href="/Nosotros">Nosotros</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  )
+}
+
+export default Header;
