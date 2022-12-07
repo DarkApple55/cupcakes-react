006831843c63411c24b283cdd4540c9c73f3d595
DarkApple55 <axelcy@gmail.com>
2022-12-07 20:43:13 -0300

    Single-page application

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../styles/App.css"
+
 import Header from "./sections/Header"
 import Home from "./pages/Home"
 import Cupcakes from "./pages/Cupcakes"
+import Nosotros from "./pages/Nosotros"
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 const App = () => {
 
   return(
-    <>
+    <Router>
       <Header/>
-      <Home/>
-      <Cupcakes/>
-    </>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/Cupcakes" element={<Cupcakes/>}></Route>
+        <Route path="/Nosotros" element={<Nosotros/>}></Route>
+      </Routes>
+    </Router>
   )
 }
 
--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -1,15 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../../styles/sections/Header.css"
+import { NavLink } from "react-router-dom"
 
 const Header = () => {
   return(
-    <nav className="navbar navbar-expand-lg bg-light Header-header">
+  <nav className="navbar navbar-expand-lg bg-light Header-header">
     <div className="container-fluid Header-container">
-      <a className="navbar-brand" href="/">Inicio</a>
+      {/* <NavLink className="navbar-brand" to="/">Inicio</NavLink> */}
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item"><a className="nav-link active" href="/Cupcakes">Cupcakes</a></li>
-          <li className="nav-item"><a className="nav-link active" href="/Nosotros">Nosotros</a></li>
+          <li className="nav-item"><NavLink className="nav-link active" to="/">Inicio</NavLink></li>
+          <li className="nav-item"><NavLink className="nav-link active" to="/Nosotros">Nosotros</NavLink></li>
+          <li className="nav-item"><NavLink className="nav-link active" to="/Cupcakes">Cupcakes</NavLink></li>
         </ul>
       </div>
     </div>
