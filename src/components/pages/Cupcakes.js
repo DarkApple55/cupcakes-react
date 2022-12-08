ef534f0c72b2e5c9e46ab468fa40ea6d3d26650a
DarkApple55 <axelcy@gmail.com>
2022-12-08 16:14:14 -0300

    async / await

--- src/components/pages/Cupcakes.js
+++ src/components/pages/Cupcakes.js
@@ -4,29 +4,30 @@ import "../../styles/pages/Cupcakes.css"
 import { useEffect, useState } from "react"
 import Cupcake from "../cards/Cupcake"
 
+// fetch("http://localhost:3001/Cupcakes").then(response => response.json()).then(data => setCupcakes(data))
+const db_GetCupcakes = async() => {
+    const response = await fetch("http://localhost:3002/Cupcakes")
+    const data = await response.json()
+    return data
+}
 
 const Cupcakes = () => {
 
   const [Cupcakes, setCupcakes] = useState([])
-  useEffect(() => {
-    fetch("http://localhost:3001/Cupcakes").then(response => response.json()).then(data => setCupcakes(data))
-  }, [])
+  useEffect(() => {db_GetCupcakes().then(data => setCupcakes(data))}, [])
 
   return(
     <header className='Cupcakes-header'>
       <div className='Cupcakes-titles'>
         <h1>Lista de Cupcakes</h1>
       </div>
-      {Cupcakes ? (
       <section className='Cupcakes-list'>
-        {Cupcakes.map(({color, sabor, descripcion, precio, id}) => (
-          <Cupcake key={id} color={color} sabor={sabor} descripcion={descripcion} precio={precio} id={id}/>
-        ))}
-      </section>) : 
-      (<span>Cargando...</span>)}
+        {Cupcakes[0] ? (Cupcakes.map(({color, sabor, descripcion, precio, id}) => (
+            <Cupcake key={id} color={color} sabor={sabor} descripcion={descripcion} precio={precio} id={id} />
+          ) )) : (<span>Cargando...</span>)}
+      </section>
     </header>
   )
 }
-
 
 export default Cupcakes;
